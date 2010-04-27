c4f262f6f0d22a8e2bb2f3e1b41b56bb2e59a7e5
Rachel A. Foecking <rfoeckin@lab03.cs.trincoll.edu>
2010-04-27 16:44:19 -0400

    Fixed dao so it actually works, added some display functionality

--- model/dao.php
+++ model/dao.php
@@ -40,23 +40,40 @@ global $global;
  	function execute($query)
  	{
  		//optionally show the query if desired
- 		if(debug_show_queries)
- 		{
- 			global $query_count;
- 			echo (++$query_count). " $query<br />\n";
- 		}
+ 		//if(debug_show_queries)
+ 		//{
+ 		//	global $query_count;
+ 		//	echo (++$query_count). " $query<br />\n";
+ 		//}
  		return $this->db->Execute($query);
  	}
 	function saveFind($guid, $projectId, $description, $barcode_id, $name, $latitude, $longitude, $revision, $imei)
 	{
-		global $global;
-		$db=$global["db"];
 		$sql="INSERT INTO `posit_find` ( `id` , `project_id` , `description` , `barcode_id` , `name` , `add_time` , `modify_time` , `latitude` ,
 		`longitude` , `revision` , `imei` )
 		VALUES (
 		'" . $guid . "', '" . $projectid . "', '" . $description . "', '" . $barcode_id . "',
 		'" . $name . "', '', '', '" . $latitude . "', '" . $longitude . "', '" . $revision . "', 
 		'" . $imei . "')";
-		$msg = $db->Execute($sql);
+		$result = $this->execute($sql);
+	}
+	function getAllFinds()
+	{
+		$sql = "SELECT * FROM `posit_find`";
+		$result = $this->execute($sql);
+		while(!$result->EOF)
+ 		{
+ 			$finds['id'][] = $result->fields['id'];
+			$finds['projectId'][] = $result->fields['project_id'];
+			$finds['description'][] = $result->fields['description'];
+			$finds['barcodeId'][] = $result->fields['barcode_id'];
+			$finds['name'][] = $result->fields['name'];
+			$finds['latitude'][] = $result->fields['latitude'];
+			$finds['longitude'][] = $result->fields['longitude'];
+			$finds['revision'][] = $result->fields['revision'];
+			$finds['imei'][] = $result->fields['imei'];
+ 			$result->moveNext();
+ 		}
+		return $finds;
 	}
 }
